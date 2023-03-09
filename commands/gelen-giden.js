fa74a9264b1021192967142d53b62bdcf893c067
Crew <113601819+CrewLua@users.noreply.github.com>
2023-03-09 19:38:37 +0300

    Create gelen-giden.js

---
+++ commands/gelen-giden.js
@@ -0,0 +1,31 @@
+module.exports = {
+    kod: 'gelen-giden',
+    run (client, message, args) {
+        if (!args[0]) return message.reply(`**${crew.prefix}gelen-giden ayarla/sıfırla**`)
+        if (args[0] === "ayarla") {
+            let kanal = message.mentions.channels.first() || message.guild.channels.cache.get(args[1])
+            if (!kanal) return message.reply(`Bir kanal belirtiniz.`)
+
+            db.set(`gelen_giden_${message.guild.id}`)
+
+            const embed = new MessageEmbed()
+            .setDescription(`Başarıyla gelen-giden kanalı ${kanal} olarak ayarlandı.`)
+            .setColor('RANDOM')
+            .setFooter('Crew 🖤')
+            message.reply({ embeds : [embed] })
+        }
+
+        if (args[0] === "sıfırla") {
+            let kontrol = db.fetch(`gelen_giden_${message.guild.id}`)
+            if (!kontrol) return message.reply(`Gelen Giden sistemi zaten kapalı`)
+
+            db.delete(`gelen_giden_${message.guild.id}`)
+            
+            const embed = new MessageEmbed()
+            .setDescription(`Başarıyla gelen-giden sistem kapatıldı.`)
+            .setColor('RANDOM')
+            .setFooter('Crew 🖤')
+            message.reply({ embeds : [embed] })
+        }
+    }
+}
